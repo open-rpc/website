5b0dd8482ef8384eaba62cc44eb4c2b80c632a91
Zane Starr <zcstarr@gmail.com>
2025-10-31 15:51:35 -0700

    chore: add additional eslint,vercel, and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tseslint from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'prettier': prettierPlugin
+    },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -13,6 +20,8 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  prettierConfig
+
 ]);
 
 export default eslintConfig;
