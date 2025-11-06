177695a12f5bc649d3174468919255e1a8be3079
Zane Starr <zcstarr@gmail.com>
2025-11-05 16:55:36 -0800

    feat: init nextra build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
-
+/*
 const nextConfig: NextConfig = {
-  /* config options here */
 };
 
 export default nextConfig;
+*/
+
+import nextra from 'nextra'
+
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+
+  defaultShowCopyCode: true,
+  latex: true,
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+
+  // ... Add regular Next.js options here
+})
---
+++ src/content/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+  index: 'Introduction',
+  'getting-started': 'Getting Started',
+  use: 'Use Cases',
+  beginners: 'Beginners Guide',
+  developers: 'Developers',
+  learn: 'Learn More'
+}
